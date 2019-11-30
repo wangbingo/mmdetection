e4c1605acfcb8cc149fa1385a74ab69518cc600c
wangbing <wangbing@static.kpn.net>
2019-12-01 01:05:40 +0800

    ms-train 2

--- configs/hrnet/faster_rcnn_hrnetv2p_w18_1x_ms-train.py
+++ configs/hrnet/faster_rcnn_hrnetv2p_w18_1x_ms-train.py
@@ -184,7 +184,7 @@ log_config = dict(
 total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/faster_rcnn_hrnetv2p_w18_1x_test'
+work_dir = './work_dirs/faster_rcnn_hrnetv2p_w18_1x_ms-train'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
