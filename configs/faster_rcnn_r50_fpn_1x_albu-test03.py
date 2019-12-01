23384011f0472d1c396ce08663659d422a2648ba
wangbing <wangbing@106.1.68.92.in-addr.arpa>
2019-12-01 19:47:39 +0800

    albu test 03

--- configs/faster_rcnn_r50_fpn_1x_albu-test03.py
+++ configs/faster_rcnn_r50_fpn_1x_albu-test03.py
@@ -226,7 +226,7 @@ log_config = dict(
 total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/faster_rcnn_r50_fpn_1x_coco-test'
+work_dir = './work_dirs/faster_rcnn_r50_fpn_1x_albu-test03'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
