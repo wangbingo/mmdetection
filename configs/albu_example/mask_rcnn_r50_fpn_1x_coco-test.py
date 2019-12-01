9b89331e3bde0c26f48b2823b1a931564e775972
wangbing <wangbing@106.1.68.92.in-addr.arpa>
2019-12-01 16:39:12 +0800

    del some albu_train_transforms

--- configs/albu_example/mask_rcnn_r50_fpn_1x_coco-test.py
+++ configs/albu_example/mask_rcnn_r50_fpn_1x_coco-test.py
@@ -124,44 +124,14 @@ albu_train_transforms = [
         rotate_limit=0,
         interpolation=1,
         p=0.5),
-    dict(
-        type='RandomBrightnessContrast',
-        brightness_limit=[0.1, 0.3],
-        contrast_limit=[0.1, 0.3],
-        p=0.2),
-    dict(
-        type='OneOf',
-        transforms=[
-            dict(
-                type='RGBShift',
-                r_shift_limit=10,
-                g_shift_limit=10,
-                b_shift_limit=10,
-                p=1.0),
-            dict(
-                type='HueSaturationValue',
-                hue_shift_limit=20,
-                sat_shift_limit=30,
-                val_shift_limit=20,
-                p=1.0)
-        ],
-        p=0.1),
-    dict(type='JpegCompression', quality_lower=85, quality_upper=95, p=0.2),
     dict(type='ChannelShuffle', p=0.1),
-    dict(
-        type='OneOf',
-        transforms=[
-            dict(type='Blur', blur_limit=3, p=1.0),
-            dict(type='MedianBlur', blur_limit=3, p=1.0)
-        ],
-        p=0.1),
 ]
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
     dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
     dict(type='Pad', size_divisor=32),
-    """ dict(
+    dict(
         type='Albu',
         transforms=albu_train_transforms,
         bbox_params=dict(
@@ -176,7 +146,7 @@ train_pipeline = [
             'gt_bboxes': 'bboxes'
         },
         update_pad_shape=False,
-        skip_img_without_anno=True), """
+        skip_img_without_anno=True),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='DefaultFormatBundle'),
     dict(
