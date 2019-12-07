540235f5206ba0d2d24c0e1b350107d28613e2eb
wangbing <wangbing@static.kpn.net>
2019-12-07 18:50:52 +0800

    frozen_stages=4

--- configs/dcn/cascade_rcnn_dconv_c5_r50_fpn_1x_test.py
+++ configs/dcn/cascade_rcnn_dconv_c5_r50_fpn_1x_test.py
@@ -8,7 +8,7 @@ model = dict(
         depth=50,
         num_stages=4,
         out_indices=(0, 1, 2, 3),
-        frozen_stages=1,
+        frozen_stages=4,
         style='pytorch',
         dcn=dict(
             modulated=False, deformable_groups=1, fallback_on_stride=False),
@@ -209,7 +209,7 @@ data = dict(
         img_prefix=data_root + 'images/',
         pipeline=test_pipeline))
 # optimizer
-optimizer = dict(type='SGD', lr=0.002, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.0025, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
@@ -217,7 +217,7 @@ lr_config = dict(
     warmup='linear',
     warmup_iters=500,
     warmup_ratio=1.0 / 3,
-    step=[16, 23])
+    step=[8, 11])
 checkpoint_config = dict(interval=1)
 # yapf:disable
 log_config = dict(
@@ -229,10 +229,10 @@ log_config = dict(
 # yapf:enable
 evaluation = dict(interval=1)
 # runtime settings
-total_epochs = 24
+total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 work_dir = './work_dirs/cascade_rcnn_dconv_c5_r50_fpn_1x_test'
 load_from = None
-resume_from = './work_dirs/cascade_rcnn_dconv_c5_r50_fpn_1x_test/epoch_12.pth'
+resume_from = None
 workflow = [('train', 1)]
