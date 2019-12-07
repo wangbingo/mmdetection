ff838ec49c632d50fa8550cf07e7f1441c2d987c
wangbing <wangbing@static.kpn.net>
2019-12-07 22:11:40 +0800

    400x400 for val

--- configs/hrnet/faster_rcnn_hrnetv2p_w18_1x_albu-ShiftScaleRotate.py
+++ configs/hrnet/faster_rcnn_hrnetv2p_w18_1x_albu-ShiftScaleRotate.py
@@ -161,7 +161,7 @@ albu_train_transforms = [
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(1400, 1400), keep_ratio=True),         ###
+    dict(type='Resize', img_scale=(400, 400), keep_ratio=True),         ###
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
