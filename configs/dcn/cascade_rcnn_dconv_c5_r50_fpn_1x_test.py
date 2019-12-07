d75e3d02714b4a30411c069ffc8594a94e1b1136
wangbing <wangbing@static.kpn.net>
2019-12-07 14:10:11 +0800

    resume  epoch_2

--- configs/dcn/cascade_rcnn_dconv_c5_r50_fpn_1x_test.py
+++ configs/dcn/cascade_rcnn_dconv_c5_r50_fpn_1x_test.py
@@ -234,5 +234,5 @@ dist_params = dict(backend='nccl')
 log_level = 'INFO'
 work_dir = './work_dirs/cascade_rcnn_dconv_c5_r50_fpn_1x_test'
 load_from = None
-resume_from = None
+resume_from = './work_dirs/cascade_rcnn_dconv_c5_r50_fpn_1x_test/epoch_2.pth'
 workflow = [('train', 1)]
