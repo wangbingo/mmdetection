3eb1eec021bf5dcd9d561d8f498ef86e7b171e40
wangbing <wangbing@static.kpn.net>
2019-12-07 21:06:25 +0800

    cas dcn c5 24e

--- configs/dcn/cascade_rcnn_dconv_c5_r50_fpn_1x_test.py
+++ configs/dcn/cascade_rcnn_dconv_c5_r50_fpn_1x_test.py
@@ -8,7 +8,7 @@ model = dict(
         depth=50,
         num_stages=4,
         out_indices=(0, 1, 2, 3),
-        frozen_stages=4,
+        frozen_stages=3,
         style='pytorch',
         dcn=dict(
             modulated=False, deformable_groups=1, fallback_on_stride=False),
@@ -217,7 +217,7 @@ lr_config = dict(
     warmup='linear',
     warmup_iters=500,
     warmup_ratio=1.0 / 3,
-    step=[8, 11])
+    step=[20, 23])
 checkpoint_config = dict(interval=1)
 # yapf:disable
 log_config = dict(
@@ -229,10 +229,10 @@ log_config = dict(
 # yapf:enable
 evaluation = dict(interval=1)
 # runtime settings
-total_epochs = 12
+total_epochs = 24
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 work_dir = './work_dirs/cascade_rcnn_dconv_c5_r50_fpn_1x_test'
 load_from = None
-resume_from = None
+resume_from = './work_dirs/cascade_rcnn_dconv_c5_r50_fpn_1x_test/epoch_12.pth'
 workflow = [('train', 1)]
