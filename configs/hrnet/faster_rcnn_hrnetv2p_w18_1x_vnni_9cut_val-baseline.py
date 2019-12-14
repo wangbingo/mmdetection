921d7c2d715dbd24ea8d0b4707c334eccf68043b
wangbing <wangbing@static.kpn.net>
2019-12-15 01:27:40 +0800

    no neg

--- configs/hrnet/faster_rcnn_hrnetv2p_w18_1x_vnni_9cut_val-baseline.py
+++ configs/hrnet/faster_rcnn_hrnetv2p_w18_1x_vnni_9cut_val-baseline.py
@@ -115,13 +115,13 @@ test_cfg = dict(
 )
 # dataset settings
 dataset_type = 'CocoDataset'
-data_root = '/root/docker_mounts_sata/pap_work_vnni/dataset/line01/cut_9/coco/'
+data_root = '/root/docker_mounts_sata/pap_work_vnni/dataset/line01/cut_9/coco/'       ###
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(1200, 1200), keep_ratio=True),
+    dict(type='Resize', img_scale=(1200, 1200), keep_ratio=True),       ###
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -132,7 +132,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1200, 1200),
+        img_scale=(1200, 1200),       ###
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
@@ -150,7 +150,7 @@ data = dict(
         type=dataset_type,
         ann_file=[
             data_root + 'annotations/pos_train.json',
-            data_root + 'annotations/neg_size80to85_0.3times_train.json'
+            #data_root + 'annotations/neg_size80to85_0.3times_train.json'
         ],
         img_prefix=data_root + 'images/',
         pipeline=train_pipeline),
@@ -158,7 +158,7 @@ data = dict(
         type=dataset_type,
         ann_file=[
             data_root + 'annotations/pos_valid.json',
-            data_root + 'annotations/neg_size80to85_0.3times_valid.json'
+            #data_root + 'annotations/neg_size80to85_0.3times_valid.json'
         ],
         img_prefix=data_root + 'images/',
         pipeline=test_pipeline),
