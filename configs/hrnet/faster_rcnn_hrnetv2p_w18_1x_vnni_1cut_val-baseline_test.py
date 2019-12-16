322f9b57708f6182b7f58edf1edebfc36cc43cdb
wangbing <wangbing@static.kpn.net>
2019-12-16 23:21:45 +0800

    300,300

--- configs/hrnet/faster_rcnn_hrnetv2p_w18_1x_vnni_1cut_val-baseline_test.py
+++ configs/hrnet/faster_rcnn_hrnetv2p_w18_1x_vnni_1cut_val-baseline_test.py
@@ -132,7 +132,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1200, 1200),       ###
+        img_scale=(300, 300),       ###
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
