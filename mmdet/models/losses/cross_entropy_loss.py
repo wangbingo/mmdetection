50bb2d243df52691bec84dac518e12db2d75677b
wangbing <wangbing@static.kpn.net>
2019-12-24 23:16:08 +0800

    5000-500  beta=0.999

--- mmdet/models/losses/cross_entropy_loss.py
+++ mmdet/models/losses/cross_entropy_loss.py
@@ -90,9 +90,9 @@ def cross_entropy(pred, label, weight=None, reduction='mean', avg_factor=None):
     no_of_classes = 2
     logits = torch.rand(10,no_of_classes).float()
     labels = torch.randint(0,no_of_classes, size = (10,))
-    beta = 0.5             #   0.9 ~ 0.9999(longtail)
+    beta = 0.999             #   0.9 ~ 0.9999(longtail)
     gamma = 2.0            #   0.5, 1.0, 2.0
-    samples_per_cls = [1, 1]
+    samples_per_cls = [5000, 500]
     loss_type = "focal"
     cb_loss = CB_loss(labels, logits, samples_per_cls, no_of_classes,loss_type, beta, gamma)
     loss = cb_loss
