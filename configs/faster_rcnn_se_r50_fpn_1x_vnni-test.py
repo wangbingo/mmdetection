546f15756a3fcf3501ba1722aa48684985a8a3bc
wangbing <wangbing@static.kpn.net>
2019-12-29 22:48:04 +0800

    frozen_stages=4,

--- configs/faster_rcnn_se_r50_fpn_1x_vnni-test.py
+++ configs/faster_rcnn_se_r50_fpn_1x_vnni-test.py
@@ -4,7 +4,10 @@ model = dict(
     pretrained='work_dirs/pre_train/se_resnet50-ce0d4300.pth',
     backbone=dict(
         type='SENet',
-        block='SEResNetBottleneck', layers=[3, 4, 6, 3], groups=1, reduction=16,
+        block='SEResNetBottleneck', 
+        layers=[3, 4, 6, 3],
+        frozen_stages=4, 
+        groups=1, reduction=16,
         dropout_p=0.2, num_classes=3),
     neck=dict(
         type='FPN',
