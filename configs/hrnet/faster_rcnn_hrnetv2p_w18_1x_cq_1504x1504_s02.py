f9245d863666c3cd19c66eab0111381fa1cf56a1
wangbing <wangbing@static.kpn.net>
2019-12-30 21:52:24 +0800

    1504x1504,dir

--- configs/hrnet/faster_rcnn_hrnetv2p_w18_1x_cq_1504x1504_s02.py
+++ configs/hrnet/faster_rcnn_hrnetv2p_w18_1x_cq_1504x1504_s02.py
@@ -184,7 +184,7 @@ log_config = dict(
 total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/faster_rcnn_hrnetv2p_w18_1x_cq_2048x1504_s02'
+work_dir = './work_dirs/faster_rcnn_hrnetv2p_w18_1x_cq_1504x1504_s02'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
