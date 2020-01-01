bd116df0d665fecb5b741468f0aec7d2b5f7ddea
wangbing <wangbing@static.kpn.net>
2020-01-02 00:06:16 +0800

    1504->1888

--- configs/dcn/cascade_rcnn_dconv_c4-c5_r50_fpn_1x_cq_1888_s22.py
+++ configs/dcn/cascade_rcnn_dconv_c4-c5_r50_fpn_1x_cq_1888_s22.py
@@ -168,7 +168,7 @@ img_norm_cfg = dict(
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(1504, 1504), keep_ratio=True),          ###
+    dict(type='Resize', img_scale=(1888, 1888), keep_ratio=True),          ###
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -179,7 +179,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1504, 1504),                  ###
+        img_scale=(1888, 1888),                  ###
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
