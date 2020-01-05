a9736dfe0d09c8eafeb7be39e7674bc4d04392e4
wangbing <wangbing@static.kpn.net>
2020-01-05 19:07:15 +0800

    s38S  cross

--- mmdet/models/losses/cross_entropy_loss.py
+++ mmdet/models/losses/cross_entropy_loss.py
@@ -9,8 +9,8 @@ from .utils import weight_reduce_loss
 
 def cross_entropy(pred, label, weight=None, reduction='mean', avg_factor=None):
     
-    #-->class loss          38L
-    coe = [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.2, 1.2, 1.5, 0.0, 0.0, 1.0]
+    #-->class loss          38S
+    coe = [1.0, 3.2, 0.8, 0.8, 0.3, 1.1, 0.0, 0.0, 0.0, 0.5, 0.3, 1.0]
     #      0    1    2    3    4    5    6     7    8    9    10   11
     
     list1 = []
