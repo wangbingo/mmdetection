1a3378ffd2e03e5b83afebf4f30cb5d2e68e19ff
wangbing <wangbing@static.kpn.net>
2020-01-10 13:18:28 +0800

    3008->4096

--- configs/fcos/fcos_r50_caffe_fpn_gn_1x_cq_4096_s62.py
+++ configs/fcos/fcos_r50_caffe_fpn_gn_1x_cq_4096_s62.py
@@ -60,7 +60,7 @@ img_norm_cfg = dict(
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(3008, 3008), keep_ratio=True),  ###
+    dict(type='Resize', img_scale=(4096, 4096), keep_ratio=True),  ###
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -71,7 +71,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(3008, 3008),          ###
+        img_scale=(4096, 4096),          ###
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
@@ -128,7 +128,7 @@ log_config = dict(
 total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/fcos_r50_caffe_fpn_gn_1x_cq_3008_s62'
+work_dir = './work_dirs/fcos_r50_caffe_fpn_gn_1x_cq_4096_s62'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
