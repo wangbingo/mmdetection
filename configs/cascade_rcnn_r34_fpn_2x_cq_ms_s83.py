234712da06b0a9a51f225cc689f87f82281977d7
wangbing <wangbing@static.kpn.net>
2020-01-15 20:11:51 +0800

    s83

--- configs/cascade_rcnn_r34_fpn_2x_cq_ms_s83.py
+++ configs/cascade_rcnn_r34_fpn_2x_cq_ms_s83.py
@@ -228,7 +228,7 @@ log_config = dict(
 total_epochs = 24
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/cascade_rcnn_r18_fpn_2x_cq_ms_s82'
+work_dir = './work_dirs/cascade_rcnn_r34_fpn_2x_cq_ms_s83'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
