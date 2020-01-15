d09d9653a24c5906510be9c456a23cea811ab921
wangbing <wangbing@101.1.68.92.in-addr.arpa>
2020-01-15 17:56:18 +0800

    2x

--- configs/cascade_rcnn_r18_fpn_2x_cq_ms_s82.py
+++ configs/cascade_rcnn_r18_fpn_2x_cq_ms_s82.py
@@ -225,7 +225,7 @@ log_config = dict(
     ])
 # yapf:enable
 # runtime settings
-total_epochs = 50
+total_epochs = 24
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 work_dir = './work_dirs/cascade_rcnn_r18_fpn_2x_cq_ms_s82'
